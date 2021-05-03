c49151ddf381f95bae643ed6ab5b1a184a0d0730
Pat <sipapat.prasopsap@k-mit.se>
2021-05-03 09:14:13 +0200

    fixed bugs

--- src/controllers/schedules.js
+++ src/controllers/schedules.js
@@ -1,5 +1,4 @@
 const { scheduleModel } = require('../models/Schedule');
-const { extraScheduleModel } = require('../models/ExtraSchedule');
 const ErrorResponse = require('../utils/errorResponse');
 
 const scheduleController = {
