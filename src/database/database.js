33da021185b2fcb30a25cd0c975ecdad69bb8c0f
pontus <sipapat.prasopsap@gmail.com>
2023-01-08 22:57:19 +0100

    commit

--- src/database/database.js
+++ src/database/database.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
+    console.time('connect');
     await mongoose.connect(`mongodb+srv://${process.env.DBHOST}:${process.env.DBPASSWORD}@patpra.2jv8s.mongodb.net/${process.env.DBNAME}?retryWrites=true&w=majority`, {
       useNewUrlParser: true,
       useCreateIndex: true,
@@ -10,6 +11,7 @@ const connectDB = async () => {
       useUnifiedTopology: true,
     });
     console.log('Connect to DB');
+    console.timeEnd('connect');
   } catch (error) {
     console.error(`Database connecting error : ${error}`);
   }
